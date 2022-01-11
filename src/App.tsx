ba8db18dad7884d95f83716c4774365c95a18ec4
André gois <andre.s.gois3@gmail.com>
2022-01-10 23:54:19 -0300

    installing styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: #8257e6;
+`
 
 export function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Title>Hello world</Title>
     </div>
   );
 }
