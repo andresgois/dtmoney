0196999317f27442f58e2511612f04213b259d74
André gois <andre.s.gois3@gmail.com>
2022-01-12 00:06:00 -0300

    working with styles and miragejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import { Header } from "./components/Header";
-import { GlobalStyle } from "./styles/global"
+import { Dashboard } from "./components/Dashboard";
+import { GlobalStyle } from "./styles/global";
+import { createServer } from "miragejs";
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+});
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
